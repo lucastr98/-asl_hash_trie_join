d7a77bd9c0dc81b7742e6550315668e4ec04bd31
Luca Strässle <lucastr@student.ethz.ch>
2023-10-28 17:04:16 +0200

    remove synthetic sql data bcs unused

--- sql/synthetic/load.sql
+++
@@ -1,3 +0,0 @@
-COPY r FROM '/tmp/synthetic-data/R.csv' WITH (FORMAT csv, DELIMITER '|');
-COPY s FROM '/tmp/synthetic-data/S.csv' WITH (FORMAT csv, DELIMITER '|');
-COPY t FROM '/tmp/synthetic-data/T.csv' WITH (FORMAT csv, DELIMITER '|');
